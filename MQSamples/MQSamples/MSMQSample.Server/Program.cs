07a8ea3e765797096291d2019af9c110d09619c6
Chemmy <chengming0916@gmail.com>
2019-10-21 14:58:16 +0800

    添加MSMQ示例

---
+++ MQSamples/MQSamples/MSMQSample.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQSample.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //消息队列路径
+            string path = ".\\Private$\\myQueue";
+            MessageQueue messageQueue;
+            if (MessageQueue.Exists(path))
+            {
+                messageQueue = new MessageQueue(path);
+            }
+            else
+            {
+                messageQueue = MessageQueue.Create(path);
+            }
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Message message = messageQueue.Receive();//消息为阻塞模式
+                    message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+                    string str = message.Body.ToString();
+                    Console.WriteLine("接收消息--->" + str + "--->" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff"));
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MQSamples/MQSamples/MSMQSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Messaging;
+using System.Timers;
+
+namespace MSMQSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //消息队列路径
+            string path = ".\\Private$\\myQueue";
+
+            MessageQueue messageQueue;
+            if (!MessageQueue.Exists(path))
+            {
+                messageQueue = MessageQueue.Create(path);
+            }
+            else
+            {
+                messageQueue = new MessageQueue(path);
+            }
+            Timer timer = new Timer();
+            timer.Interval = TimeSpan.FromSeconds(2).TotalMilliseconds;
+            timer.Elapsed += (s, e) =>
+            {
+                Message message = new Message()
+                {
+                    Body = "Hello",
+                    Formatter = new XmlMessageFormatter(new Type[] { typeof(string) })
+                };
+                Console.WriteLine("发送消息--->" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff"));
+
+                messageQueue.Send(message);
+            };
+            timer.Start();
+            Console.ReadKey();
+        }
+    }
+}
