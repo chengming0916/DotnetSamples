a47cb7214eb6be98c80dad8fbbce27eb3e536d6b
Chemmy <chengming0916@outlook.com>
2022-02-28 09:27:12 +0800

    更新thrift示例

--- RPCSamples/ThriftSample.DTO/Thirft/textCsharp.thrift
+++ RPCSamples/ThriftSample.DTO/Thirft/textCsharp.thrift
@@ -1,6 +1,8 @@
-namespace java com.javabloger.gen.code   #  注释1
+namespace csharp Thrift.Sample.DTO   #  csharp 命名控件定义
 
-struct Blog {   #  注释2 
+# include "pe1.thrift" # 添加引用
+
+struct Blog {   #  自定义类型声明
     1: string topic 
     2: binary content 
     3: i64    createdTime 
@@ -10,8 +12,8 @@ struct Blog {   #  注释2
   }
 
 
-service ThriftCase {  #  注释3 
-    i32 testCase1(1:i32 num1, 2:i32 num2, 3:string  num3)  #  注释4
+service ThriftCase {  #  service 类定义 
+    i32 testCase1(1:i32 num1, 2:i32 num2, 3:string  num3)  #  函数定义
 
     list<string> testCase2(1:map<string,string>  num1)
 
