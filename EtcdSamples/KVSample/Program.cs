79c87de77dad7554f52ea3f80a02dc8e374b707a
Chemmy <chengming0916@outlook.com>
2023-03-15 19:02:35 +0800

    提娜佳Etcd基础示例

--- EtcdSamples/KVSample/Program.cs
+++ EtcdSamples/KVSample/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using dotnet_etcd;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace KVSample
 {
@@ -10,6 +12,40 @@ namespace KVSample
     {
         static void Main(string[] args)
         {
+            var client = new EtcdClient("tcp://127.0.0.1");
+
+            var timer = new Timer();
+            timer.Interval = TimeSpan.FromSeconds(5).TotalMilliseconds;
+            timer.Elapsed += (s, e) =>
+            {
+                client.Put("/EtcdSamples/KVSample/Now", DateTime.Now.ToString()); //写入数据
+            };
+            timer.Start();
+
+            var timer1 = new Timer(); //每秒读取一次
+            timer1.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds;
+            timer1.Elapsed += (s, e) =>
+            {
+                var result = client.GetVal("/EtcdSamples/KVSample/Now"); //读取数据
+                Console.WriteLine(result);
+            };
+            timer1.Start();
+
+            client.Put("/EtcdSamples/KVSample/Delete", "TEST");
+            Console.WriteLine(client.GetVal("/EtcdSamples/KVSample/Delete"));
+
+            Console.ReadKey();
+
+            var response = client.Delete("/EtcdSamples/KVSample/Delete");
+            if (response.Deleted)
+            {
+
+            }
+            Console.WriteLine(client.GetVal("/EtcdSamples/KVSample/Delete"));
+
+            Console.ReadKey();
+
+
         }
     }
 }
