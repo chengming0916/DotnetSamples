7a736c3eec8f72686c3e4f7a3bf2bc36e9e90796
Chemmy <chengming0916@outlook.com>
2024-04-01 16:03:05 +0800

    添加WPF异常梳理示例

---
+++ ExceptionHandleSamples/Exception.Handle.WPF/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Exception.Handle.WPF
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                if (e.ExceptionObject is System.Exception)
+                {
+                    // log 
+                }
+            }
+            catch (System.Exception ex)
+            {
+                //TODO: log exception and show error report view
+                //throw;
+            }
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+            }
+            catch (System.Exception)
+            {
+                //TODO: log exception and show error report view
+                //throw;
+            }
+        }
+    }
+}
